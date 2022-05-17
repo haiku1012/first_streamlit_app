7773bad51e88c87cacb3a9a6c7a1124096641632
haiku1012 <105742038+haiku1012@users.noreply.github.com>
2022-05-17 23:08:45 +0530

    Update streamlit_app.py
    added

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,5 @@ import streamlit
 streamlit.title('My parents healthy new diner')
 
 streamlit.title('it was a pleasant day to start with!')
+
+streamlit.text('day started with cup of tea and loaf of bread.')
